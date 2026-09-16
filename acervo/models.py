291a95ff9388c381df50d3f8d736cde8eccc753e
Daniel Daigo <daniel.daigo@example.com>
2026-09-16 20:48:17 -0300

    Adição do campo tipo_acervo ao model

--- acervo/models.py
+++ acervo/models.py
@@ -3,10 +3,16 @@ from django.db import models
 # Create your models here.
 
 class Livro(models.Model):
+    TIPO_ACERVO_CHOICES = (
+        ('Fisico', 'Físico'),
+        ('Digital', 'Digital'),
+    )
+    
     titulo = models.CharField(max_length=200)
     autor = models.CharField(max_length=100)
     ano = models.IntegerField()
     disponivel = models.BooleanField(default=True)
+    tipo_acervo = models.CharField(max_length=10, choices=TIPO_ACERVO_CHOICES, default='Fisico')
 
     def __str__(self):
         return self.titulo
